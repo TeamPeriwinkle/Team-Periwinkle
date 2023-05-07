991516818e81ec01c4ff0ab9ddf27a60d0d53375
TeamPeriwinkle <129917654+TeamPeriwinkle@users.noreply.github.com>
2023-05-06 19:48:36 -0700

    Iteration1

--- DIYControl.java
+++ DIYControl.java
@@ -8,28 +8,33 @@ public class DIYControl extends JFrame {
     private JLabel usernameLabel;
     private JTextField usernameField;
     private JLabel passwordLabel;
-    private JPasswordField passwordField;
-    private JLabel confirmPassLabel;
-    private JPasswordField confirmPassField;
+    //private JPasswordField passwordField;
+    private JTextField passwordField;
+    private JPanel buttonsPanel;
     private JButton loginButton;
     private JButton signUpButton;
     private JButton backButton;
+    private JButton aboutButton;
     private JButton exitButton;
+    private JPanel aboutPanel;
+    private String firstname;
+    private String email;
+    private GridBagConstraints gbc;
 
     public DIYControl() {
-        super("DIY Control");
+        super("DIYControl");
     }
    
     public void display() {
-        setSize(500, 400);
+        setSize(500, 500);
 
         // Set layout
         setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
+        gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
          
         // Title label
-        titleLabel = new JLabel("Welcome to DIY Control");
+        titleLabel = new JLabel("Welcome to DIYControl");
         titleLabel.setFont(new Font("", Font.BOLD, 24));
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -37,7 +42,7 @@ public class DIYControl extends JFrame {
         add(titleLabel, gbc);
 
         // Username label and field
-        usernameLabel = new JLabel("Username:");
+        usernameLabel = new JLabel("Firstname:");
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.gridwidth = 1;
@@ -49,34 +54,28 @@ public class DIYControl extends JFrame {
         add(usernameField, gbc);
 
         // Password label and field
-        passwordLabel = new JLabel("Password:");
+        passwordLabel = new JLabel("Email Address:");
         gbc.gridx = 0;
         gbc.gridy = 2;
         add(passwordLabel, gbc);
 
-        passwordField = new JPasswordField(20);
+        passwordField = new JTextField(20);
         gbc.gridx = 1;
         gbc.gridy = 2;
         add(passwordField, gbc);
         
-       //  JLabel forgotPasswordLabel = new JLabel("Forgot Username or Password?");
-//          gbc.gridx = 1;
-//          gbc.gridy = 3;
-//          add(forgotPasswordLabel,gbc);
-// 
-//         // Login and sign up buttons
-        
-        JPanel buttonsPanel = new JPanel();
+        buttonsPanel = new JPanel();
         buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS));
         gbc.gridx = 0;
         gbc.gridy = 4;
         gbc.gridwidth = 4;
         add(buttonsPanel, gbc);
         
-        loginButton = new JButton("Login");
+        loginButton = new JButton("Create");
         signUpButton = new JButton("Sign Up");
         backButton = new JButton("Back");
         exitButton = new JButton("Exit");
+        aboutButton = new JButton("About");
         exitButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
@@ -87,51 +86,63 @@ public class DIYControl extends JFrame {
         loginButton.setMaximumSize(buttonSize);
         backButton.setMaximumSize(buttonSize);
         exitButton.setMaximumSize(buttonSize);
-        buttonsPanel.add(signUpButton);
+        aboutButton.setMaximumSize(buttonSize);
+        //buttonsPanel.add(signUpButton);
         buttonsPanel.add(loginButton);
+        buttonsPanel.add(Box.createRigidArea(new Dimension(0, 10)));
         buttonsPanel.add(exitButton);
-        
-        confirmPassField = new JPasswordField(20);
-        confirmPassLabel = new JLabel("Confirm Password:");
         
         loginButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             
-                titleLabel.setText("Login!!!");
-                remove(usernameLabel);
-                remove(usernameField);
-                remove(passwordLabel);
-                remove(passwordField);
-                revalidate();
-                repaint();
+                firstname = usernameField.getText();
+                email = passwordField.getText();
                 
-                buttonsPanel.remove(signUpButton);
-                buttonsPanel.remove(loginButton);
+                if (firstname.compareTo("") == 0 || email.compareTo("") == 0) {
+                    JOptionPane.showMessageDialog(getParent(), "Please enter your Firstname and email address", "DIYControl", JOptionPane.WARNING_MESSAGE);
+                } else {
+                    titleLabel.setText("Welcome " + firstname + "!");
+                    remove(usernameLabel);
+                    remove(usernameField);
+                    remove(passwordLabel);
+                    remove(passwordField);
+                    revalidate();
+                    repaint();
+                    
+                    buttonsPanel.remove(loginButton);
+                    buttonsPanel.add(aboutButton);
+                    buttonsPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+                    buttonsPanel.add(exitButton);
+                }
             
             }
         });
         
-        signUpButton.addActionListener(new ActionListener() {
+        aboutButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             
-                titleLabel.setText("Create an account");
-                usernameField.setText("");
-                passwordField.setText("");
-                GridBagConstraints gbc = new GridBagConstraints();
-                gbc.insets = new Insets(10, 10, 10, 10);
-                gbc.fill = GridBagConstraints.HORIZONTAL;
+                String firstname = usernameField.getText();
+                String email = passwordField.getText();
+                
+                titleLabel.setText("About");
+                buttonsPanel.remove(aboutButton);
+                buttonsPanel.remove(exitButton);
+                
+                About a = new About(firstname, email);
+                a.setup();
+                aboutPanel = a.getPanel();
                 
                 gbc.gridx = 0;
-                gbc.gridy = 3;
+                gbc.gridy = 1;
                 gbc.gridwidth = 1;
-                add(confirmPassLabel, gbc);
+                add(aboutPanel, gbc);
                 
-                gbc.gridx = 1;
-                gbc.gridy = 3;
-                add(confirmPassField, gbc);
-                
-                buttonsPanel.remove(loginButton);
+                gbc.gridx = 0;
+                gbc.gridy = 2;
+                gbc.gridwidth = 1;
+                add(buttonsPanel, gbc);
                 buttonsPanel.add(backButton);
+                buttonsPanel.add(Box.createRigidArea(new Dimension(0, 10)));
                 buttonsPanel.add(exitButton);
             
             }
@@ -139,18 +150,20 @@ public class DIYControl extends JFrame {
         
         backButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                titleLabel.setText("Welcome to DIY Control");
-                usernameField.setText("");
-                passwordField.setText("");
-                confirmPassField.setText("");
-                remove(confirmPassLabel);
-                remove(confirmPassField);
+            
+                titleLabel.setText("Welcome " + firstname + "!");
+                remove(aboutPanel);
                 revalidate();
                 repaint();
                 
+                gbc.gridx = 0;
+                gbc.gridy = 1;
+                gbc.gridwidth = 1;
+                add(buttonsPanel, gbc);
                 buttonsPanel.remove(backButton);
                 buttonsPanel.remove(exitButton);
-                buttonsPanel.add(loginButton);
+                buttonsPanel.add(aboutButton);
+                buttonsPanel.add(Box.createRigidArea(new Dimension(0, 10)));
                 buttonsPanel.add(exitButton);
             
             }
@@ -161,6 +174,11 @@ public class DIYControl extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         
+    }
+    
+    public static void main(String[] theArgs) {
+    	DIYControl frame = new DIYControl();
+    	frame.display();
     }
 
 }
